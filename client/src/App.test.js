b1a3eb82abf75c54bd9ff5b75d6a5f75d3ccd291
deborah1993 <deborah.sanchez1993@gmail.com>
2022-05-02 11:57:10 -0300

    Terminado

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -3,13 +3,16 @@ import App from "./App";
 import { Provider } from "react-redux";
 import { BrowserRouter as Router } from "react-router-dom";
 import Landing from "./components/Landing/Landing";
-import createStore from "redux";
 import store from "./store/index";
+import { createStore } from "redux";
 
 describe("App debe renderizar Landing y ButtonHome", () => {
-  let store = createStore();
-
-  render(<App />);
+  // render(<App />);
   //const linkElement = screen.getByText("links");
   //expect(linkElement).toBeInTheDocument();
+  it("should have the correct className", () => {
+    const { container } = render(<App></App>);
+
+    expect(container.firstChild).toHaveClass("app");
+  });
 });
